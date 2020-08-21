d7abda0bc24c051431944355d57209b6e573e602
kharioki <antolisah@gmail.com>
2020-08-21 03:52:15 +0300

    added navigation and implement bottom navigation

--- App.js
+++ App.js
@@ -5,20 +5,121 @@
  * @format
  * @flow strict-local
  */
-
+import 'react-native-gesture-handler';
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-import Home from './src/pages/Home';
+import {View, Text, TouchableOpacity, Image} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createStackNavigator} from '@react-navigation/stack';
 
-const App = () => {
-  return <Home />;
-};
+import Home from './src/pages/Home';
+import {colors} from './src/constants';
+
+const Tab = createBottomTabNavigator();
+
+function TestScreen() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Test Screen</Text>
+    </View>
+  );
+}
+
+// tabs
+function MyTabBar({state, descriptors, navigation}) {
+  const focusedOptions = descriptors[state.routes[state.index].key].options;
+
+  if (focusedOptions.tabBarVisible === false) {
+    return null;
+  }
+
+  return (
+    <View style={{flexDirection: 'row'}}>
+      {state.routes.map((route, index) => {
+        const {options} = descriptors[route.key];
+        const label =
+          options.tabBarLabel !== undefined
+            ? options.tabBarLabel
+            : options.title !== undefined
+            ? options.title
+            : route.name;
+
+        const isFocused = state.index === index;
+
+        const onPress = () => {
+          const event = navigation.emit({
+            type: 'tabPress',
+            target: route.key,
+            canPreventDefault: true,
+          });
+
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name);
+          }
+        };
+
+        const onLongPress = () => {
+          navigation.emit({
+            type: 'tabLongPress',
+            target: route.key,
+          });
+        };
+
+        const getIcons = () => {
+          if (label == 'Home') {
+            return require('./src/assets/icons/flower.png');
+          }
+          if (label == 'Heart') {
+            return require('./src/assets/icons/heart-icon.png');
+          }
+          return require('./src/assets/icons/user-icon.png');
+        };
+
+        return (
+          <TouchableOpacity
+            accessibilityRole="button"
+            accessibilityStates={isFocused ? ['selected'] : []}
+            accessibilityLabel={options.tabBarAccessibilityLabel}
+            testID={options.tabBarTestID}
+            onPress={onPress}
+            onLongPress={onLongPress}
+            style={{
+              flex: 1,
+              padding: 10,
+              alignItems: 'center',
+              paddingBottom: 34,
+            }}>
+            <Image
+              source={getIcons()}
+              style={{tintColor: isFocused ? colors.base : '#d0d3d0'}}
+            />
+          </TouchableOpacity>
+        );
+      })}
+    </View>
+  );
+}
+
+function MyTabs() {
+  return (
+    <Tab.Navigator tabBar={props => <MyTabBar {...props} />}>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Heart" component={TestScreen} />
+      <Tab.Screen name="User" component={TestScreen} />
+    </Tab.Navigator>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator headerMode="none">
+        <Stack.Screen name="HomeTab" component={MyTabs} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 export default App;
